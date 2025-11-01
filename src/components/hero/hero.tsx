3a85632740c1f45b520cd6f6ec92f63f74b25e53
Patrick MacDonald <patrick@patmac.ca>
2025-11-01 17:47:43 -0600

    some css

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 export default function Hero() {
 	return (
 		<div className="bg-[url('/hero.png')] bg-cover bg-top w-full md:w-5/6 h-160 grid grid-cols-2">
-			<div className="col-span-1 flex flex-col  items-center justify-center  text-4xl font-bold gap-4 pl-4 md:pl-8">
+			<div className="col-span-2 sm:col-span-1 flex flex-col  items-center justify-end mb-8 md:justify-center   text-4xl font-bold gap-4 pl-4 md:pl-8">
 				<h1>Need Work Done On Your House?</h1>
                 <h1>We Are Reliable And Consistence</h1>
                 <Button className="mt-4">Get a Quote Today</Button>
--- src/components/nav/nav-bar.tsx
+++ src/components/nav/nav-bar.tsx
@@ -27,36 +27,13 @@ const components: { title: string; href: string; description: string }[] = [
 		description:
 			'For sighted users to preview content available behind a link.',
 	},
-	// {
-	// 	title: 'Progress',
-	// 	href: '/docs/primitives/progress',
-	// 	description:
-	// 		'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-	// },
-	// {
-	// 	title: 'Scroll-area',
-	// 	href: '/docs/primitives/scroll-area',
-	// 	description: 'Visually or semantically separates content.',
-	// },
-	// {
-	// 	title: 'Tabs',
-	// 	href: '/docs/primitives/tabs',
-	// 	description:
-	// 		'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-	// },
-	// {
-	// 	title: 'Tooltip',
-	// 	href: '/docs/primitives/tooltip',
-	// 	description:
-	// 		'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-	// },
-]
+	]
 
 export function NavBar() {
 	// const isMobile = useIsMobile()
 
 	return (
-		<NavigationMenu viewport={false}>
+		<NavigationMenu viewport={true}>
 			<NavigationMenuList className="flex-wrap">
 				<NavigationMenuItem>
 					<NavigationMenuLink
