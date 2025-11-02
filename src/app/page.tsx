f7d57d2201bec4024b4c460b2586803f2f91183c
Patrick MacDonald <patrick@patmac.ca>
2025-11-02 13:34:39 -0700

    css updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,16 @@
-import Image from 'next/image'
+import { ContentSectionOne } from '@/components/content-sections/content-section-one'
 import Hero from '@/components/hero/hero'
 
 export default function Home() {
 	return (
 		<div className="flex flex-col gap-16 w-full items-center">
 			<Hero />
-			<ContentSection />
+			<ContentSectionOne />
+			<ContentSectionTwo />
 		</div>
 	)
 }
 
-function ContentSection() {
-	return (
-		<div className="grid grid-cols-2 w-full md:w-5/6">
-			<div className="flex justify-center items-center">
-				<h1 className="text-black ml-2 md:ml-8 text-lg md:text-4xl font-bold ">
-					Let Us Deal With Your Home Renovation Needs.
-				</h1>
-			</div>
-			<div className="w-full justify-end flex ">
-				<Image
-					alt={'Reno Image'}
-					className='className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px] bg-blue-200 p-2"'
-					height={600}
-					src={'/reno.png'}
-					width={600}
-				/>
-			</div>
-		</div>
-	)
+function ContentSectionTwo() {
+	return <div className="w-full bg-secondary-foreground"></div>
 }
---
+++ src/components/content-sections/content-section-one.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+export function ContentSectionOne() {
+	return (
+		<div className="sm:grid sm:grid-cols-2 flex flex-col w-full md:w-5/6">
+			<div className="flex justify-center items-center">
+				<h1 className="text-black ml-2 md:ml-8 text-4xl font-bold text-center">
+					Let Us Deal With Your Home Renovation Needs.
+				</h1>
+			</div>
+			<div className="w-full justify-end flex">
+				<Image
+					alt={'Reno Image'}
+					className=' border shadow-2xl'
+					height={600}
+					src={'/reno1.png'}
+					width={600}
+				/>
+			</div>
+		</div>
+	)
+}
